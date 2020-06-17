b94ca80564b629bad3dec39a7f857e331857ef47
Sharmistha Parui <paruisharmistha72@gmail.com>
2020-06-17 18:57:11 +0530

    cors fix

--- expense_tracker/backend/server.js
+++ expense_tracker/backend/server.js
@@ -14,6 +14,10 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 require("./config/passport")(passport);
 
