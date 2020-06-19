ab4c428510f8295f5c4b8fd41c6bd7bd606922ed
Sharmistha Parui <paruisharmistha72@gmail.com>
2020-06-19 21:34:19 +0530

    fixing refresh problem

--- expense_tracker/backend/server.js
+++ expense_tracker/backend/server.js
@@ -44,7 +44,7 @@ if(process.env.NODE_ENV === 'production')
 
   app.get('*',(req,res)=>{
 
-    res.sendFile(path.resolve(__dirname,'expense_tracker','build','index.html'));
+    res.sendFile(path.resolve(__dirname +'/../build/index.html'));
   });
 }
 
