2cf5f9d83136cd08efb5f586f078345466c7d573
Sharmistha Parui <paruisharmistha72@gmail.com>
2020-06-20 09:37:54 +0530

    removed redux dev tool

--- expense_tracker/src/components/register_user.component.js
+++ expense_tracker/src/components/register_user.component.js
@@ -31,12 +31,12 @@ class Register extends Component {
       });
     }
   }
-  componentDidMount() {
+ /* componentDidMount() {
     // If logged in and user navigates to Register page, should redirect them to dashboard
     if (this.props.auth.isAuthenticated) {
       this.props.history.push("/dashboard");
     }
-  }
+  }*/
   onChange = e => {
     this.setState({ [e.target.id]: e.target.value });
   };
--- expense_tracker/src/store.js
+++ expense_tracker/src/store.js
@@ -9,8 +9,7 @@ const store=createStore(
     rootReducer,
     intialState,
     compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(...middleware)
       )
 );
 export  default store;
