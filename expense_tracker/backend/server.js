3343b3b7daa8a3b73bd4d8287d2fd90701ec12ac
Sharmistha Parui <paruisharmistha72@gmail.com>
2020-06-20 13:35:42 +0530

    server changes

--- expense_tracker/backend/server.js
+++ expense_tracker/backend/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app=express();
 const port=process.env.PORT || 7000;
-const hostname = process.env.HOST || 'localhost';
+//const hostname = process.env.HOST || 'localhost';
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
@@ -49,8 +49,8 @@ if(process.env.NODE_ENV === 'production')
   });
 }
 
-app.listen(port,hostname,() =>{
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port,() =>{
+  console.log(`Server running at :${port}/`);
 });
 process.on('unhandledRejection', (reason, promise) => {
     console.log('Unhandled Rejection at:', promise, 'reason:', reason);
